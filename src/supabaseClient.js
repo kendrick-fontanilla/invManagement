f83f79a72ea97ec5afe5af3aa221cd3cb9b98082
Your Name <your-email@example.com>
2025-05-29 09:10:27 +0800

    final commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your actual Supabase project details
-const supabaseUrl = 'https://owminiqfnntwzxkgyuhq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93bWluaXFmbm50d3p4a2d5dWhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NDU2NTQsImV4cCI6MjA2MjQyMTY1NH0.KhRHbq3NYzJGTVC2mGhbhRRhSK-P27MQk5ZZfrZhe3M'
+const supabaseUrl = 'https://iradphcrwwokdrnhxpnd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyYWRwaGNyd3dva2Rybmh4cG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MTI5ODEsImV4cCI6MjA2MjI4ODk4MX0.X1okOgCMPHNh_vufxDnSlENTO99tMDjkSOXMeWawNrU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
